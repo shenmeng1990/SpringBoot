4db4e73be94a6d959398138b53c483d8f9bd12ad
shenmeng <852051958@qq.com>
2020-06-14 13:21:48 +0800

    redis双重检测锁

--- 03-ssrm-jsp/src/main/java/com/alibaba/model/Student.java
+++ 03-ssrm-jsp/src/main/java/com/alibaba/model/Student.java
@@ -2,12 +2,16 @@ package com.alibaba.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @Author shenmeng
  * @Date 2020/6/12
  **/
 @Data
-public class Student {
+public class Student implements Serializable{
+
+    private static final long serialVersionUID = -322578097227500103L;
 
     private Integer id;
 
