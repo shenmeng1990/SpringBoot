d5dd10faa226408949240f92828666d15a4b7309
shenmeng <852051958@qq.com>
2020-06-22 11:14:40 +0800

    'thymeleaf'

---
+++ 08-path/src/main/java/com/alibaba/dao/StudentDao.java
@@ -0,0 +1,18 @@
+package com.alibaba.dao;
+
+import com.alibaba.model.Student;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author shenmeng
+ * @Date 2020/6/12
+ **/
+@Mapper
+public interface StudentDao {
+
+    void insertStudent(Student student);
+
+    Student selectById(int id);
+
+    Integer count();
+}
